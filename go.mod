c8726444011b74e83fd2725a5823f534d83b8779
Venukishore-R <venuvk0304@gmail.com>
2025-07-11 23:25:08 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_health_check
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func main() {
+	http.HandleFunc("/health", healthCheckHandler)
+
+	log.Println("Server starting on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
